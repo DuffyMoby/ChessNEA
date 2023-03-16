e88454ba00ea99f8f83bbcdf201add382cc0eb66
Jason <mobyman701@gmail.com>
2023-03-16 12:24:27 +0800

    Added halfKP feature set

--- nn/model.py
+++ nn/model.py
@@ -5,9 +5,12 @@ from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 from argparse import ArgumentParser
 import pickle as pkl
+import numpy as np
 import tensorboard
 
+FEATURE_SIZE_HALFKP = 40960
 SCALING_FACTOR = 410
+
 class BIGDATA(Dataset):
     def __init__(self, positions) -> None:
         self.bitboards = positions['bitboard']
@@ -22,7 +25,8 @@ class BIGDATA(Dataset):
         bitboard, eval = torch.from_numpy(bitboard), torch.as_tensor(eval)
         return bitboard, eval
 
-class Sparse_set(Dataset):
+
+class sparse_set1_v2(Dataset):
     def __init__(self, positions) -> None:
         self.indices = positions['indices']
         self.stms = positions['stm']
@@ -33,18 +37,85 @@ class Sparse_set(Dataset):
 
     def __getitem__(self, idx):
         indices = self.indices[idx]
-        values = torch.ones(len(indices), dtype=torch.float32)
+        indices = np.expand_dims(indices, 0)
+        values = torch.ones(indices.shape[1], dtype=torch.float32)
 
         eval = self.centipawns[idx]
         eval = torch.as_tensor(eval, dtype=torch.float32)
 
-        stm = torch.as_tensor(self.stms[idx], dtype=torch.float32)
+        stm = self.stms[idx]
+        stm = torch.as_tensor(stm, dtype=torch.float32)
 
-        bitboard = torch.sparse_coo_tensor([indices], values, [768])
+        bitboard = torch.sparse_coo_tensor(indices, values, [768])
+        bitboard = bitboard.to_dense()
         
         return bitboard, eval, stm
 
 
+class sparse_halfKP(Dataset):
+    def __init__(self, positions) -> None:
+        super().__init__()
+        self.white_feature_indices = positions['white_feature_indices']
+        self.black_feature_indices = positions['black_feature_indices']
+        self.num_active_features = positions['num_active_features'].astype(int)
+        self.stms = positions['stms'].astype(int)
+        self.centipawns = positions['centipawns']
+    
+    def __len__(self):
+        return self.centipawns.shape[0]
+
+    def __getitem__(self, idx):
+        num_active_features = self.num_active_features[idx]
+        num_active_features = torch.as_tensor(num_active_features)
+
+        white_feature_indices = self.white_feature_indices[idx]
+        white_feature_indices = torch.from_numpy(white_feature_indices[:num_active_features])
+
+        black_feature_indices = self.black_feature_indices[idx]
+        black_feature_indices = torch.from_numpy(black_feature_indices[:num_active_features])
+
+        stm = self.stms[idx]
+        stm = torch.as_tensor([stm])
+
+        eval = self.centipawns[idx]
+        eval = torch.as_tensor([eval], dtype=torch.float32)
+
+        return white_feature_indices, black_feature_indices, num_active_features, stm, eval
+
+def collate_fn_halfkp(batch):
+    white_feature_indices, black_feature_indices, num_active_features, stm, eval = zip(*batch) 
+    # Dataloader returns a list of tuples from sampling dataset -> zip used to accumulate each item
+    white_feature_indices = torch.cat(white_feature_indices)
+    black_feature_indices = torch.cat(black_feature_indices)
+    num_active_features = torch.tensor(num_active_features)
+    stm = torch.stack(stm, dim=0)
+    eval = torch.stack(eval, dim=0)
+    # turn list of tensors into single tensor 
+    batch_size = eval.shape[0]
+    batch_range = torch.arange(batch_size)
+    position_indices = torch.repeat_interleave(batch_range, num_active_features)
+    values = torch.ones(torch.sum(num_active_features))
+    white_feature_indices = torch.stack((position_indices, white_feature_indices), dim=0)
+    black_feature_indices = torch.stack((position_indices, black_feature_indices), dim=0)
+    # stack turns tensor into shape (2, sum(num_active_features)) so it can be input into sparse tensor
+    # since sparse coordinate tensor takes indices in this form
+    # indices -> [[dimension1],
+    #             [dimension2]]
+    # resulting in a tensor of shape (dimension1, dimension2)
+    white_feature_tensor = torch.sparse_coo_tensor(
+        indices=white_feature_indices,
+        values=values,
+        size=(batch_size, FEATURE_SIZE_HALFKP)
+    )
+    black_feature_tensor = torch.sparse_coo_tensor(
+        indices=black_feature_indices,
+        values=values,
+        size=(batch_size, FEATURE_SIZE_HALFKP)
+    )
+
+    return white_feature_tensor, black_feature_tensor, stm, eval
+
+                                                                                            
 class BigNet(nn.Module):
     def __init__(self, in_size) -> None:
         super().__init__()
@@ -77,7 +148,36 @@ class SmallNet(nn.Module):
         )
     def forward(self, x):
         return self.layerStack(x)
+    
 
+class halfKP_Net(nn.Module):
+    def __init__(self, in_size, L_0_size, L_1_size, L_2_size) -> None:
+        super().__init__()
+        self.L_0_white = nn.Sequential(
+            nn.Linear(in_size, L_0_size),
+            nn.ReLU()
+            )
+        self.L_0_black = nn.Sequential(
+            nn.Linear(in_size, L_0_size),
+            nn.ReLU()
+            )
+        self.layerStack = nn.Sequential(
+            nn.Linear(2*L_0_size, L_1_size),
+            nn.ReLU(),
+            nn.Linear(L_1_size, L_2_size),
+            nn.ReLU(),
+            nn.Linear(L_2_size, 1)
+        )
+    def forward(self, white_features_in, black_features_in, stm):
+        w_0 = self.L_0_white(white_features_in)
+        b_0 = self.L_0_black(black_features_in)
+        
+        acc = (stm * torch.cat([w_0, b_0], dim=1)) + ((1 - stm) * torch.cat([b_0, w_0], dim=1))
+        # concatenate w_0 and b_0 such that if white to move - Layer_1 gets input [w_0, b_0], black to move - Layer_1 gets input [b_0, w_0]
+        out = self.layerStack(acc)
+
+        return out
+            
 
 class nnEval_model_set1_v1(pl.LightningModule):
     def __init__(self, net, learning_rate = 0.001) -> None:
@@ -88,13 +188,13 @@ class nnEval_model_set1_v1(pl.LightningModule):
 
     def training_step(self, batch):
         x, y = batch
+        y = torch.reshape(y, (-1, 1)) # reshapes to tensor of size (batchsize, 1)
         wdl_eval_pred = torch.sigmoid(self.net(x) / SCALING_FACTOR) #Convert to WDL space. 
         # Sigmoid bounds values between 0-1.
         # Scaling Factor scales data such that most values lie in the range [-4,4]
         # This means large evals are much closer together
         # And there is a larger difference between more average evals found in real play. 
         wdl_eval_target = torch.sigmoid(y / SCALING_FACTOR)
-        wdl_eval_target = torch.reshape(wdl_eval_target, (-1, 1))
         loss = self.lossfn(wdl_eval_pred, wdl_eval_target)
         self.log("train_loss", loss) # log to tensorboard
         return loss
@@ -113,15 +213,11 @@ class nnEval_model_set1_v2(pl.LightningModule):
 
     def training_step(self, batch):
         x, y, stm = batch
-        stm = torch.reshape(stm, (-1, 1)) # converts to 2d tensor of shape (batchsize, 1))
+        stm = torch.reshape(stm, (-1, 1)) 
         y = torch.reshape(y, (-1, 1)) 
-        y_hat = self.net(x) * (stm + (1 - stm)) # negates eval if black to move
+        y_hat = self.net(x) * (stm + (stm - 1)) # negates eval if black to move
 
-        wdl_eval_pred = torch.sigmoid(y_hat / SCALING_FACTOR) #Convert to WDL space. 
-        # Sigmoid bounds values between 0-1.
-        # Scaling Factor scales data such that most values lie in the range [-4,4]
-        # This means large evals are much closer together
-        # And there is a larger difference between more average evals found in real play. 
+        wdl_eval_pred = torch.sigmoid(y_hat / SCALING_FACTOR) 
         wdl_eval_target = torch.sigmoid(y / SCALING_FACTOR)
 
         loss = self.lossfn(wdl_eval_pred, wdl_eval_target)
@@ -132,21 +228,48 @@ class nnEval_model_set1_v2(pl.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         return optimizer
 
-if __name__ == "__main__":
-    chess_path = os.path.abspath('Datasets/bitboardeval_sparse.pkl')
-    with open(chess_path, 'rb') as f:
-        chess_dataset_dict = pkl.load(f)
 
-    chess_dataset = Sparse_set(chess_dataset_dict)
-    train_dataloader = DataLoader(chess_dataset, batch_size=512, num_workers=0)
+class nnEval_model_halfKP(pl.LightningModule):
+    def __init__(self, net, learning_rate = 0.001) -> None:
+        super().__init__()
+        self.net = net
+        self.lr = learning_rate
+        self.lossfn = torch.nn.MSELoss()
+
+    def training_step(self, batch):
+        white_feature_tensor, black_feature_tensor, stm, eval = batch
+        eval_pred = self.net(white_feature_tensor, black_feature_tensor, stm)
+        
+        wdl_eval_pred = torch.sigmoid(eval_pred / SCALING_FACTOR)
+        wdl_eval_target = torch.sigmoid(eval / SCALING_FACTOR)
+
+        loss = self.lossfn(wdl_eval_pred, wdl_eval_target)
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+    
+
+if __name__ == "__main__":
+    chess_path = './Datasets/halfKP_evals_sparse.npz'
+    f = open(chess_path, 'rb')
+    chess_dataset_dict = np.load(f)
+    # with open(chess_path, 'rb') as f:
+    #     chess_dataset_dict = pkl.load(f)
+
+    chess_dataset = sparse_halfKP(chess_dataset_dict)
+    train_dataloader = DataLoader(chess_dataset, batch_size=4096, num_workers=0, collate_fn=collate_fn_halfkp)
     # train_length = len(chess_dataset) // 1.25
     # test_length = len(chess_dataset) - train_length
     parser = ArgumentParser()
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args()
 
-    model = nnEval_model_set1_v2(SmallNet(768))
+    model = nnEval_model_halfKP(halfKP_Net(FEATURE_SIZE_HALFKP, 256, 256, 256))
     trainer = pl.Trainer.from_argparse_args(args)
     trainer.fit(model, train_dataloader)
+    f.close()
 
 
--- nn/parse.py
+++ nn/parse.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from math import exp
 import numpy as np
 from tqdm import trange
 import pickle
+from copy import deepcopy
 
 ONETOEIGHT = set(map(str, range(1,9)))
 PIECE_TABLE = {
@@ -27,7 +27,8 @@ COLOR_TABLE = {
 
 def fen2bitboard(fen):
     board, color, *_ = fen.split()
-    board = board.replace("/", "")
+    board = board.split("/")[::-1]
+    board = "".join(board)
     sq = 0
     out = np.zeros(768, dtype=np.float32)
     for pc in board:
@@ -41,9 +42,10 @@ def fen2bitboard(fen):
     return out
 
 
-def fen2bitboard_sparse(fen):
+def fen2bitboard_sparse_set1_v2(fen):
     board, color, *_ = fen.split()
-    board = board.replace("/", "")
+    board = board.split("/")[::-1]
+    board = "".join(board)
     sq = 0
     out_indices = []
     for pc in board:
@@ -54,10 +56,43 @@ def fen2bitboard_sparse(fen):
             sq += 1
             out_indices.append(idx)
 
-    return out_indices, COLOR_TABLE[color]
+    return np.array(out_indices, dtype=np.int64), COLOR_TABLE[color]
 
+
+def fen2halfKP_sparse(fen, max_active_features):
+    board, color, *_ = fen.split()
+    board = board.split("/")[::-1]
+    board = "".join(board)
+    w_king_sq = 0
+    b_king_sq = 0
+    sq = 0
+    num_active_features = 0
+    piece_indices = np.zeros(max_active_features, dtype=np.int64)
+    for pc in board:
+        if pc == 'K':
+            w_king_sq = sq
+            sq += 1
+
+        elif pc == 'k':
+            b_king_sq = sq
+            sq += 1
+
+        elif pc in ONETOEIGHT:
+            sq += int(pc)
+
+        else:
+            idx = PIECE_TABLE[pc][0] * 64 + sq
+            sq += 1
+            piece_indices[num_active_features] = idx
+            num_active_features += 1
+
+    white_indices = deepcopy(piece_indices)
+    black_indices = deepcopy(piece_indices)
+
+    white_indices[:num_active_features] = white_indices[:num_active_features] + (w_king_sq * 10) * 64
+    black_indices[:num_active_features] = black_indices[:num_active_features] + (b_king_sq * 10) * 64
+    return white_indices, black_indices, num_active_features, COLOR_TABLE[color]
     
-
 def transformCP(val):
     if val[0] == '#':
         return int(val[1] + '5000')
@@ -72,6 +107,8 @@ def transformCP(val):
 def main_set1_v1(data_path, out_path):
     """
     For first version of feature set 1
+    Stores whole bitboard input along with its eval
+    Results in a rather unnecesarily large file -> 40GB!!!
     """
     chess_data = pd.read_csv(data_path)
     num_entries = len(chess_data)
@@ -90,27 +127,58 @@ def main_set1_v1(data_path, out_path):
         pickle.dump(big, f)
 
 
-def main_sparse_set1_v2(data_path, out_path):
+def main_sparse_set1_v2(fen_transform, data_path, out_path):
     """
     For second version of feature set 1
+    Stores indices of non zero bits (only ones)
+    Results in much smaller file size since feature set is very sparse (<5% non zero values)
     """
     chess_data = pd.read_csv(data_path)
     num_entries = len(chess_data)
     d = {
-        'indices' : [],
+        'indices' : np.ndarray(num_entries, dtype=np.object_),
         'stm' : np.ndarray(num_entries), # np arrays are marginally faster to load
         'centipawn' : np.ndarray(num_entries, dtype=np.float32)
     }
     for i in trange(num_entries):
         centipawn = transformCP(chess_data.iloc[i, 1])
-        indices, stm = fen2bitboard_sparse(chess_data.iloc[i, 0])
+        indices, stm = fen_transform(chess_data.iloc[i, 0])
 
-        d['indices'].append(indices)
+        d['indices'][i] = indices
         d['stm'][i] = stm
         d['centipawn'][i] = centipawn
 
     with open(out_path, 'wb') as f:
         pickle.dump(d, f)
+
+def main_sparse_HALFKP(fen_transform, data_path, out_path):
+    """
+    For second version of feature set 1
+    Stores indices of non zero bits (only ones)
+    Results in much smaller file size since feature set is very sparse (<5% non zero values)
+    """
+    chess_data = pd.read_csv(data_path)
+    num_entries = len(chess_data)
+    max_features = 30
+
+    white_feature_indices = np.ndarray((num_entries, max_features), dtype=np.int64)
+    black_feature_indices = np.ndarray((num_entries, max_features), dtype=np.int64)
+    num_active_features = np.ndarray(num_entries, dtype=int)
+    stms = np.ndarray(num_entries, dtype=int) # np arrays are marginally faster to load
+    centipawns = np.ndarray(num_entries, dtype=np.float32)
+    
+    for i in trange(num_entries):
+        centipawn = transformCP(chess_data.iloc[i, 1])
+        white_indices, black_indices, num_active, stm = fen_transform(chess_data.iloc[i, 0], max_features)
+
+        white_feature_indices[i] = white_indices
+        black_feature_indices[i] = black_indices
+        num_active_features[i] = num_active
+        stms[i] = stm
+        centipawns[i] = centipawn
+
+    with open(out_path, 'wb') as f:
+        np.savez(f, white_feature_indices=white_feature_indices, black_feature_indices=black_feature_indices,num_active_features=num_active_features ,stms=stms, centipawns=centipawns)
 
 
 
@@ -129,8 +197,8 @@ if __name__ == '__main__':
     # transformed_data = pd.DataFrame(data=bigdict)
     # transformed_data.to_pickle('./Datasets/bitboardswitheval.pkl')
     data_path = r"./Datasets/chessData.csv"
-    out_path = r'./Datasets/bitboardeval_sparse.pkl'
-    main_sparse_set1_v2(data_path=data_path,out_path=out_path)
+    out_path = r'./Datasets/halfKP_evals_sparse.npz'
+    main_sparse_HALFKP(fen_transform=fen2halfKP_sparse, data_path=data_path,out_path=out_path)
 
 
     
